fe681979b2dcf5ac69d32928506c0f9ea9ab8e0c
EduardoIxen <tomas-1539@hotmail.com>
2020-09-18 00:10:18 -0600

    Fichas al agregadas al tablero

--- Proyecto/Controllers/HomeController.cs
+++ Proyecto/Controllers/HomeController.cs
@@ -10,22 +10,56 @@ namespace Proyecto.Controllers
     public class HomeController : Controller
     {
         private Othello_201800524Entities db = new Othello_201800524Entities();
+        public String[,] matrizTablero;
         
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
         public ActionResult Index()
         {
+            CrearTablero();
             return View();
         }
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        public void CrearTablero()
+        {
+            matrizTablero = new string[10, 10];
+            matrizTablero[0, 0] = " ";
+            matrizTablero[0, 1] = "A";
+            matrizTablero[0, 2] = "B";
+            matrizTablero[0, 3] = "C";
+            matrizTablero[0, 4] = "D";
+            matrizTablero[0, 5] = "E";
+            matrizTablero[0, 6] = "F";
+            matrizTablero[0, 7] = "G";
+            matrizTablero[0, 8] = "H";
+            matrizTablero[0, 9] = " ";
+            matrizTablero[9, 0] = " ";
+            matrizTablero[9, 1] = "A";
+            matrizTablero[9, 2] = "B";
+            matrizTablero[9, 3] = "C";
+            matrizTablero[9, 4] = "D";
+            matrizTablero[9, 5] = "E";
+            matrizTablero[9, 6] = "F";
+            matrizTablero[9, 7] = "G";
+            matrizTablero[9, 8] = "H";
+            matrizTablero[9, 9] = " ";
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (matrizTablero[i,j] == null)
+                    {
+                        matrizTablero[i, j] = " ";
+                    }
+                    if ((j == 0 || j == 9) && i < 9 && i > 0)
+                    {
+                        matrizTablero[i, j] = i.ToString();
+                    }
+                }
+            }
+            ViewBag.matriz = matrizTablero;
+            foreach (var item in matrizTablero)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
